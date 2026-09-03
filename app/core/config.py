95203086ae194083d3e0bd9dedc3a31623358c2e
AbdulQadeerKhan555 <aq272570@gmail.com>
2026-09-03 15:46:38 +0500

    updating file structure

---
+++ app/core/config.py
@@ -0,0 +1,5 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+project_name = "Trello Clone Backend"
+API_name = "/api/v1"
---
+++ app/db/session.py
@@ -0,0 +1,4 @@
+import asyncio
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+engine = create_async_engine()
