20fa77273d89b8ff33ed1d3803658c7b06e9df2b
AbdulQadeerKhan555 <aq272570@gmail.com>
2026-09-08 15:49:28 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
-# app/main.py (or app/api/v1/__init__.py)
 from fastapi import FastAPI
 from app.api.v1.auth import router as auth_router
+from app.api.v1.workspaces import router as workspaces_router
+from app.api.v1.invitations import router as invitations_router
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
 app.include_router(auth_router, prefix=settings.API_V1_STR)
+app.include_router(workspaces_router, prefix=settings.API_V1_STR)
+app.include_router(invitations_router, prefix=settings.API_V1_STR)
