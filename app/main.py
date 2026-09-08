0380fdfab2d3f093a4efeb7e327c197d1d7e8a54
AbdulQadeerKhan555 <aq272570@gmail.com>
2026-09-08 17:29:20 +0500

    updating the app/main file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
+# app/main.py
 from fastapi import FastAPI
 from app.api.v1.auth import router as auth_router
 from app.api.v1.workspaces import router as workspaces_router
 from app.api.v1.invitations import router as invitations_router
+from app.api.v1.boards import router as boards_router
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
@@ -9,3 +11,4 @@ app = FastAPI(title=settings.PROJECT_NAME)
 app.include_router(auth_router, prefix=settings.API_V1_STR)
 app.include_router(workspaces_router, prefix=settings.API_V1_STR)
 app.include_router(invitations_router, prefix=settings.API_V1_STR)
+app.include_router(boards_router, prefix=settings.API_V1_STR)
