7ac06cadeebb4790e4342e606d0741b8a4120ba4
AbdulQadeerKhan555 <aq272570@gmail.com>
2026-09-11 17:29:54 +0500

    Update board.py

--- app/models/board.py
+++ app/models/board.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import DateTime, Index, Boolean, String, func, ForeignKey, UniqueConstraint, text
+from sqlalchemy import DateTime, Index, Boolean, String, func, ForeignKey, UniqueConstraint, Text
 from sqlalchemy.dialects.postgresql import UUID
 
 from app.db.base import Base
